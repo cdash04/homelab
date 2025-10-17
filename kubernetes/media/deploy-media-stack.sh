542b55c57a092de57605f28691f2ad367ab8e400
Christophe Duchesne-Ashworth <contact@cda.sh>
2025-10-17 12:04:56 -0400

    add all new files

---
+++ kubernetes/media/deploy-media-stack.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+echo "Deploying media stack..."
+
+# Create PVCs
+kubectl apply -f jellyfin-pvc.yaml
+kubectl apply -f media-pvcs.yaml
+
+# Deploy Jellyfin
+kubectl apply -f jellyfin-deployment.yaml
+kubectl apply -f jellyfin-service.yaml
+
+# Deploy services
+kubectl apply -f transmission-deployment.yaml
+kubectl apply -f sonarr-deployment.yaml
+kubectl apply -f radarr-deployment.yaml
+kubectl apply -f bazarr-deployment.yaml
+kubectl apply -f jackett-deployment.yaml
+kubectl apply -f lidarr-deployment.yaml
+
+# Deploy secrets
+secrets_file="./secrets/jellyseer-secrets.yaml"
+if test -e "$secrets_file"; then
+    echo "$secrets_file file exists."
+    kubectl apply -f "$secrets_file"
+
+    # Deploy jellyseer
+    kubectl apply -f jellyseer-deployment.yaml
+fi
+
+# Deploy ingress
+
+kubectl apply -f media-ingress.yaml
+
+echo "Media stack deployed!"
+echo "Access URLs:"
+echo "Jellyfin: http://raspberrypi.local/jellyfin"
+echo "Radarr:   http://raspberrypi.local/radarr"
+echo "Sonarr:   http://raspberrypi.local/sonarr"
+echo "transmission: http://raspberrypi.local/transmission"
+echo "Bazarr:   http://raspberrypi.local/bazarr"
+echo "Jackett:   http://raspberrypi.local/jackett"
+echo "jellyseer:   http://raspberrypi.local:30008"
+echo "lidarr:   http://raspberrypi.local/lidarr"
